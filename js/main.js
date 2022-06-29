273c719983e74892860f9977aa41fc98e7cc70f8
Dmitry Berkashev <berkashev@exmore>
2022-06-29 17:32:02 +0300

    generate on click

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 import { getRandomString } from "./passwordgenerator.js"
 const resultField = document.getElementById("resultField")
+const generateButton = document.getElementById("generate")
 const smallLetersCheck = document.getElementById("smallLettersCheck")
 const capitalLettersCheck = document.getElementById("capitalLettersCheck")
 const numberLettersCheck = document.getElementById("numberLettersCheck")
 const stringLength = document.getElementById("stringLength")
-const randomString = getRandomString(
-	stringLength.value,
-	smallLetersCheck.checked,
-	capitalLettersCheck.checked,
-	numberLettersCheck.checked
-)
-resultField.innerText = randomString
+
+generateButton.onclick = () => {
+	const randomString = getRandomString(
+		stringLength.value,
+		smallLetersCheck.checked,
+		capitalLettersCheck.checked,
+		numberLettersCheck.checked
+	)
+	resultField.innerText = randomString
+}
