2921bb8656139d240500383c1281b6cd5584af00
Dmitry Berkashev <berkashev@exmore>
2022-07-05 17:08:47 +0300

    List of passwords

--- js/main.js
+++ js/main.js
@@ -5,13 +5,16 @@ const smallLetersCheck = document.getElementById("smallLettersCheck")
 const capitalLettersCheck = document.getElementById("capitalLettersCheck")
 const numberLettersCheck = document.getElementById("numberLettersCheck")
 const stringLength = document.getElementById("stringLength")
-
+const quantity = document.getElementById("quantity")
 generateButton.onclick = () => {
-	const randomString = getRandomString(
-		stringLength.value,
-		smallLetersCheck.checked,
-		capitalLettersCheck.checked,
-		numberLettersCheck.checked
-	)
-	resultField.innerText = randomString
+	resultField.innerHTML = ""
+	for (let i = 0; i < quantity.value; i++) {
+		const randomString = getRandomString(
+			stringLength.value,
+			smallLetersCheck.checked,
+			capitalLettersCheck.checked,
+			numberLettersCheck.checked
+		)
+		resultField.innerHTML += `<li>${randomString} </li>`
+	}
 }
